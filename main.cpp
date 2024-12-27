7d1463c909570791ea70fc4dd5e4f5d4e8103f5c
omerfazil08 <omerfazilorhan@gmail.com>
2024-12-27 20:22:03 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string name;
+	cout << "Enter your name please: ";
+	cin >> name;
+	cout <<endl<< "Welcome to our country Mr./Mrs. " << name << endl;
+	return 0;
+}
